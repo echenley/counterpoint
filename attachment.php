f17244948c2a9039bcaf526ea012349064519d38
echenley <evan.henley@gmail.com>
2014-04-20 16:03:41 -0500

    added attachment.php

---
+++ attachment.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+
+<section id="content">
+
+  <article id="attachment">
+  <?php
+  while ( have_posts() ) : the_post();
+    $photographer = get_post_meta($post->ID, 'be_photographer_name', true);
+    $photographerurl = get_post_meta($post->ID, 'be_photographer_url', true);
+    ?>
+    
+    <header class="attachment-header">
+      <h3><?php printf( __( 'Attachment: %s', 'counterpoint' ), get_the_title() ); ?></h3>
+    </header>
+    
+    <section class="post-meta">
+      <time datetime="<?php echo get_the_date('Y-m-j'); ?>" class="timestamp"><?php the_time( get_option( 'date_format' ) ); ?></time>
+    </section>
+    
+    <div class="photometa">
+      <span class="photographername"><?php echo $photographer; ?></span><a href="<?php echo $photographerurl ?>" target="_blank" class="photographerurl"><?php echo $photographerurl ?></a>
+    </div>
+    <div class="entry-attachment">
+    
+    <?php if ( wp_attachment_is_image( $post->ID ) ) : $att_image = wp_get_attachment_image_src( $post->id, "full")[0]; ?>
+      <a href="<?php echo wp_get_attachment_url($post->ID); ?>" title="<?php the_title(); ?>" rel="attachment"><img src="<?php echo $att_image; ?>"  class="attachment-medium" alt="<?php $post->post_excerpt; ?>" /></a>
+    <?php else : ?>
+      <a href="<?php echo wp_get_attachment_url($post->ID) ?>" title="<?php echo wp_specialchars( get_the_title($post->ID), 1 ) ?>" rel="attachment"><?php echo wp_basename($post->guid); ?></a>
+    <?php endif; ?>
+    </div>
+    
+  <?php endwhile; ?>
+  </article>
+  
+</section>
+
+<?php get_footer(); ?>
